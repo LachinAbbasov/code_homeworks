c409407b5018f289ce12c9cee2bae92edc3580ab
abbasov lacin <tu7n8gcpt@code.edu.az>
2024-04-26 06:01:01 +0400

    son

--- React(Todo)Task/src/Components/Todolist/index.jsx
+++ React(Todo)Task/src/Components/Todolist/index.jsx
@@ -1,13 +1,21 @@
+
+
 import React from 'react';
 import PropTypes from 'prop-types';
-
-
-const Todolist = ({ children }) => {
-  return <>{children}</>;
+import Todoitem from '../Todoitem/index'
+const Todolist = ({ todos, setTodos }) => {
+  return (
+    <div className="todo-list-container">
+      {todos.map(todo => (
+        <Todoitem key={todo.id} todo={todo} setTodos={setTodos} todos={todos} />
+      ))}
+    </div>
+  );
 };
 
 Todolist.propTypes = {
-  children: PropTypes.node,
+  todos: PropTypes.array.isRequired,
+  setTodos: PropTypes.func.isRequired,
 };
 
 export default Todolist;
